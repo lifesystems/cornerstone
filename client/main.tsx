6709a079484651644f18555d4d6cd766f6187f3f
Johannes Neugschwentner <johannes@sportskred.com>
2017-12-27 15:28:54 -0400

    milestone: hello world

--- client/main.tsx
+++ client/main.tsx
@@ -1,1 +1,14 @@
-// TODO gist this into existance
+import * as React from "react";
+import * as DOM from "react-dom";
+
+class Hello extends React.Component<object, object> {
+  render() {
+    return (
+      <div>
+        <h1>Hey!</h1>
+      </div>
+    );
+  }
+}
+
+DOM.render(<Hello />, document.getElementById("root"));
